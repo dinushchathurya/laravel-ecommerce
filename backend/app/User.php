d1b0c7df7b8c17bceeb2d0eb4866b0ca738fdaa1
dinushchathurya <dinushchathurya21@gmail.com>
2021-03-28 10:20:44 +0530

    refactor: implemented two important methods for JWT which is getJWTIdentifier(), getJWTCustomClaims().

--- backend/app/User.php
+++ backend/app/User.php
@@ -2,37 +2,25 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Model implements JWTSubject, AuthenticatableContract, AuthorizableContract
 {
-    use Notifiable;
+    use Authenticatable, Authorizable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
@@ -41,16 +29,29 @@ class User extends Authenticatable
     {
         return $this->hasMany(Product::class, 'created_by');
     }
+
     public function orders()
     {
         return $this->hasMany(Order::class, 'user_id');
     }
+
     public function shippingAddresses()
     {
         return $this->hasMany(ShippingAddress::class, 'user_id');
     }
+
     public function shoppingCart()
     {
         return $this->hasMany(ShoppingCart::class, 'user_id');
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
